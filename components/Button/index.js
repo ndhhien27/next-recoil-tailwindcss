83e41c045f66608e4ab2c6347fe20552c58f2f45
Nguyen Duc Hien <ndhienedu@gmail.com>
2021-11-03 23:52:07 +0700

    Init project

---
+++ components/Button/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Button.module.scss';
+
+const PrimaryButton = () => (
+  <button onClick={() => {}} className={styles['btn-primary']}>
+    PrimaryButton
+  </button>
+);
+
+export { PrimaryButton };
